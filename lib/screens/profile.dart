96cd6e18ba1fcb9dfda64b4501e5755ebf63bd2a
djzhang <trujunzhang@gmail.com>
2021-08-07 15:04:06 +0800

    update pubspec.xml

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -10,6 +10,7 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   static Random random = Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +22,6 @@ class _ProfileState extends State<Profile> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-
               SizedBox(height: 40),
               CircleAvatar(
                 backgroundImage: AssetImage(
@@ -31,18 +31,16 @@ class _ProfileState extends State<Profile> {
               ),
               SizedBox(height: 10),
               Text(
-                names[random.nextInt(10)],
+                "Ling Waldner",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 22,
                 ),
               ),
               SizedBox(height: 3),
-
               Text(
                 "Status should be here",
-                style: TextStyle(
-                ),
+                style: TextStyle(),
               ),
               SizedBox(height: 20),
               Row(
@@ -54,7 +52,7 @@ class _ProfileState extends State<Profile> {
                       color: Colors.white,
                     ),
                     color: Colors.grey,
-                    onPressed: (){},
+                    onPressed: () {},
                   ),
                   SizedBox(width: 10),
                   FlatButton(
@@ -63,77 +61,16 @@ class _ProfileState extends State<Profile> {
                       color: Colors.white,
                     ),
                     color: Theme.of(context).accentColor,
-                    onPressed: (){},
+                    onPressed: () {},
                   ),
-
                 ],
               ),
-
               SizedBox(height: 40),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 50),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          random.nextInt(10000).toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22,
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "Posts",
-                          style: TextStyle(
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          random.nextInt(10000).toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22,
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "Friends",
-                          style: TextStyle(
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          random.nextInt(10000).toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22,
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "Groups",
-                          style: TextStyle(
-                          ),
-                        ),
-                      ],
-                    ),
-
-                  ],
-                ),
+                child: buildStatisticInfo(),
               ),
               SizedBox(height: 20),
-
               buildPhotos(),
             ],
           ),
@@ -142,30 +79,82 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-  Widget buildPhotos(){
-   return
-
-     GridView.builder(
-       shrinkWrap: true,
-       physics: NeverScrollableScrollPhysics(),
-       primary: false,
-       padding: EdgeInsets.all(5),
-       itemCount: 15,
-       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-         crossAxisCount: 3,
-         childAspectRatio: 200 / 200,
-       ),
-       itemBuilder: (BuildContext context, int index) {
-         return Padding(
-           padding: EdgeInsets.all(5.0),
-           child: Image.asset(
-             "assets/cm${random.nextInt(10)}.jpeg",
-             fit: BoxFit.cover,
-           ),
-         );
-       },
-     );
+  Widget buildStatisticInfo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Text(
+              random.nextInt(10000).toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              "Restaurants",
+              style: TextStyle(),
+            ),
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              random.nextInt(10000).toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              "Reviews",
+              style: TextStyle(),
+            ),
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              random.nextInt(10000).toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              "Photos",
+              style: TextStyle(),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 
-
+  Widget buildPhotos() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      primary: false,
+      padding: EdgeInsets.all(5),
+      itemCount: 15,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 200 / 200,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsets.all(5.0),
+          child: Image.asset(
+            "assets/cm${random.nextInt(10)}.jpeg",
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
 }
